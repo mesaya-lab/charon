6a1dabf462c8610fccefd7a5c400b0be1f7574d0
Mesaya <mesaya@email.com>
2021-01-28 22:46:53 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const
+	path = require('path'),
+	root = path.resolve(__dirname),
+	resources = path.resolve(root, 'resources')
+
+module.exports = {
+	entry: resources,
+	mode: 'production',
+	output: {
+		filename: 'charon.js',
+		path: path.resolve(root, 'distribution')
+	},
+	resolve: {
+		modules: [resources]
+	},
+	target: 'node'
+}
